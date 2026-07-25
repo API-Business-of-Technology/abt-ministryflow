c654385437968acf643fedb90b55e7e5abdd37c7
Pierre Richard Saint Louis <saintlouispierrerichard6@gmail.com>
2026-07-25 17:50:56 -0400

    Finalisation de la page splash.

---
+++ frontend_ministry_flow/app/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import BarHeader from "./components/Bar";
+import SplashPage from "./components/Page_Splash";
+
+export default function Home() {
+  return (
+    <div>
+      <BarHeader />
+      <SplashPage />
+    </div>
+  );
+}
+
+
+
+
+
